aad49028358da61a506bc8082a6a8bfc3a7618e3
kirillkiselev-slim <kiselev99kirill1501@gmail.com>
2024-06-09 20:53:26 +0300

    add infra to the project

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,6 +94,8 @@ DATABASES = {
     }
 }
 
+CSRF_TRUSTED_ORIGINS = ['https://foodgrambykiselev.ddns.net']
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
