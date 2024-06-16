d8a3a6199e6fbcd60f858702cd5b8b78ebf33758
kirillkiselev-slim <kiselev99kirill1501@gmail.com>
2024-06-17 02:17:08 +0300

    add changes after 2nd review

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*',]
 
@@ -93,12 +93,7 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+
 
 CSRF_TRUSTED_ORIGINS = ['https://kiselevfoodgram.ddns.net']
 
