c1d465596f9aaca4f48966ee61c92c1c5a02972e
kirillkiselev-slim <kiselev99kirill1501@gmail.com>
2024-06-17 23:39:12 +0300

    add changes after 2nd review

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,9 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
